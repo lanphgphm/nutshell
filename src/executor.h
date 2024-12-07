2d73110a084a0f6c23823f2eb5c620aee1917ef6
siddankthep <quan.nguyen.210257@student.fulbright.edu.vn>
2024-12-07 16:15:16 +0700

    nutshell can see CTRL+C and +V without exiting

--- src/executor.h
+++ src/executor.h
@@ -1,33 +1,42 @@
 #ifndef EXECUTOR_H
 #define EXECUTOR_H
 
-#include<iostream> // cin, cout
-#include<unistd.h> // fork, read, write, exec, dup, access, dup2, close, 3 fds
-#include<cstdlib> // getenv
-#include<stdlib.h> // exit
-#include<sys/types.h> // pid_t, ssize_t
-#include<sstream> // istringstream
-#include<string> // string
-#include<vector> // vector
-#include<sys/wait.h> // waitpid
-#include<algorithm> // find_if
-#include<cctype> // isspace
+#include <algorithm>   // find_if
+#include <cctype>      // isspace
+#include <cstdlib>     // getenv
+#include <iostream>    // cin, cout
+#include <sstream>     // istringstream
+#include <stdlib.h>    // exit
+#include <string>      // string
+#include <sys/types.h> // pid_t, ssize_t
+#include <sys/wait.h>  // waitpid
+#include <unistd.h> // fork, read, write, exec, dup, access, dup2, close, 3 fds
+#include <vector>   // vector
 
-#include"command.h" 
+#include "command.h"
 
 class Executor {
-public: 
-    Executor(); 
-    ~Executor(); 
+  public:
+    Executor();
+    ~Executor();
 
-    int statCmd1, statCmd2 = 69; // sometimes statCmd2 unused :) 
+    int statCmd1, statCmd2 = 69; // sometimes statCmd2 unused :)
 
-    void execute(const ParsedCommand& cmd); 
-    void debug(); 
-private: 
-    void executePiped(const ParsedCommand& cmd, int& statCmd1, int& statCmd2); 
-    void executeAndOr(const ParsedCommand& cmd, int& statCmd1, int& statCmd2); 
-    void printError(); // change me 
-}; 
+    void execute(const ParsedCommand &cmd, pid_t &child_pid);
+    void debug();
 
-#endif //EXECUTOR_H
+    // for handling stopped jobs (SIGTSTP)
+    void addStoppedJob(pid_t pid);
+    pid_t getLastStoppedJob();
+    void removeStoppedJob(pid_t pid);
+    int getStoppedJobsSize();
+
+  private:
+    void executePiped(const ParsedCommand &cmd, int &statCmd1, int &statCmd2);
+    void executeAndOr(const ParsedCommand &cmd, int &statCmd1, int &statCmd2);
+    void printError(); // change me
+
+    std::vector<pid_t> stoppedJobs;
+};
+
+#endif // EXECUTOR_H
